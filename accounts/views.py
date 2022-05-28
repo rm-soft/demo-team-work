8f1ce7f76848e1d97ac8522d0d56ed53931713d2
Rubel-Mahmud <rmsoft.com@gmail.com>
2022-05-28 16:30:40 +0600

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+
+class HomeView(View):
+
+    def get(self, request):
+        return render(request, 'accounts/home.html')
